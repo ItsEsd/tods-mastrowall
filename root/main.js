20a6f0c59287c68870466f35ea6e5182149c2991
soubhikdas <d.soubhik@hotmail.com>
2024-02-07 16:39:57 +0530

    Update main.js

--- root/main.js
+++ root/main.js
@@ -1,15 +1,12 @@
-
 var url_string = window.location.href;
 var url = new URL(url_string);
 var q = window.atob(url.searchParams.get("topictd"));
 var k = window.atob(url.searchParams.get("tdkey"));
 var r = url.searchParams.get("td");
-if(r =="valid"){
-  document.getElementById('preId').value= q;
-  document.getElementById('preK').value= k;
+if (r == "valid") {
+  document.getElementById("preId").value = q;
+  document.getElementById("preK").value = k;
   readgenTOD();
+} else {
+  $("#intro").show();
 }
-else{
-  $('#intro').show();
-}
-
